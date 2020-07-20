badb40d56c4cc5673938a0f5e51d199d1bfbe146
John Delamare <deladev@protonmail.com>
2020-07-20 23:50:16 +0200

    Update app.py Implement api

--- app/wallet/src/app.py
+++ app/wallet/src/app.py
@@ -1,28 +1,24 @@
 import asyncio
+import uvloop
+import asyncpg
 import json
-import argparse
-from .models.wallet import wallet
+from aiohttp import web
+from models.wallet import wallet as wl
+
+routes = web.RouteTableDef()
+app = web.Application()
+
+@routes.post('/api/setupwallet')
+async def setup_wallet_from_seed(request):
+    request_json = await request.json()
+    input_seed = []
+    if "wallet_seed" in request_json:
+        input_seed = request_json["wallet_seed"]
+    wallet = await wl.setup_wallet_from_seed(input_seed)
+    return web.json_response(wallet)
+
+app.add_routes(routes)
 
 
-async def main():
-
-    parser = argparse.ArgumentParser(prog="WalletImplementation")
-    parser.add_argument("--new_wallet", action="store_true")
-    parser.add_argument("--new_address", action="store_true")
-    parser.add_argument("--receive_funds", action="store_true")
-    parser.add_argument("--send_funds", action="store_true")
-
-    args = parser.parse_args()
-    if args.new_wallet:
-        result = await wallet.wallet_creation()
-    elif args.new_address:
-        pass
-    elif args.receive_funds:
-        pass
-    elif args.send_funds:
-        pass
-    else:
-        print("No valid argument passed")
-
-
-asyncio.run(main())
+if __name__ == "__main__":
+    web.run_app(app)
