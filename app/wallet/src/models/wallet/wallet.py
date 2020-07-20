0a66d1ce064f83765509674df6494886c56c30f5
John Delamare <deladev@protonmail.com>
2020-07-20 23:49:13 +0200

    Update wallet.py Rewrite for web functionality through api

--- app/wallet/src/models/wallet/wallet.py
+++ app/wallet/src/models/wallet/wallet.py
@@ -1,53 +1,23 @@
-from ...utils.utils import *
+from utils.utils import validate_seed_words, response
 from ..keymanager import keymanager as km
-from ...utils.config import SEED_LENGTH
+from utils.config import SEED_LENGTH
+import re
 
 
-async def wallet_seed_request_loop():
-    ERROR_MESSAGES = {
-        "long": "To many words entered",
-        "short": "To few words entered",
-        "incorrect": "Invalid seed words",
-    }
-    while True:
-        error = None
-        wallet_seed_input = input(
-            f"Please enter your {SEED_LENGTH} word seed separated by space"
-        )
-        wallet_seed = wallet_seed_input.split(" ")
-        if len(wallet_seed) > SEED_LENGTH:
-            if wallet_seed[-1] == "":
-                # trailing space
-                wallet_seed.pop()
-                seeds_valid = validate_seed_words(wallet_seed)
-                if not seeds_valid:
-                    error = "incorrect"
-                else:
-                    break
-            else:
-                error = "long"
-        elif len(wallet_seed) < SEED_LENGTH:
-            error = "short"
-        else:
-            seeds_valid = validate_seed_words(wallet_seed)
-            if not seeds_valid:
-                error = "incorrect"
-            else:
-                break
-        if error:
-            print(ERROR_MESSAGES[error])
-
-    return wallet_seed
-
-
-async def wallet_creation():
-    # wallet from new seed or existing seed
-    restore_wallet = input("Restore from existing seed?\n")
-    if validate_input_bool(restore_wallet):
-        wallet_seed = await wallet_seed_request_loop()
-    else:
-        wallet_seed = await km.generate_new_seed()
-    keypair = await km.generate_keypair_from_seed(wallet_seed)
-
-    return {"seed": wallet_seed}
-
+async def setup_wallet_from_seed(input_seed=""):
+    wallet = []
+    wallet_seed = []
+    if input_seed:
+        if isinstance(input_seed, str):
+            wallet_seed = re.findall(r'[a-zA-z]+',input_seed)
+        elif isinstance(input_seed, list):
+            wallet_seed = input_seed
+        seed_length_difference = SEED_LENGTH - len(wallet_seed)
+        if seed_length_difference != 0:
+            return {
+                "error": f"The seed has too {'few' if seed_length_difference > 0  else 'many'} entries"
+            }
+    if not wallet_seed:
+        wallet_seed = km.generate_new_seed()
+    wallet = km.generate_keypair_from_seed(wallet_seed=wallet_seed)
+    return wallet
