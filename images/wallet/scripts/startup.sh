82192b5be0a1f1b4965fc3b743bfaadf44998d97
John Delamare <deladev@protonmail.com>
2020-07-21 17:20:39 +0200

    Adjust sleep time for database setup

--- images/wallet/scripts/startup.sh
+++ images/wallet/scripts/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 until psql "postgresql://walletuser:capitulating@walletdb/walletdb" ; do
     echo "Postgres is unavailable - sleeping"
-    sleep 15
+    sleep 8
     done
 
 adev runserver  /srv/app/api/src/ &
